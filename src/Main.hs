a564ea4247a04836a4490b629761df93e9777303
Bus000 <magnus@stavngaard.dk>
2016-09-22 23:02:09 +0200

    Remove unused imports

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,6 @@ module Main (main) where
 
 import qualified Bot
 import Control.Exception
-import Control.Monad
-import Network
-import System.IO
 
 main :: IO ()
 main = bracket Bot.connect Bot.disconnect Bot.loop
