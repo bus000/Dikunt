1532c8158903c0154ac3a75e935ccd2e77f17665
Bus000 <magnus@stavngaard.dk>
2016-09-25 00:33:37 +0200

    Flip function arguments

--- src/Functions/Parrot.hs
+++ src/Functions/Parrot.hs
@@ -6,5 +6,5 @@ import Data.List
 
 parrot :: String -> IO (Maybe String)
 parrot str
-    | str `isPrefixOf` "dikunt: " = return $ Just (drop 8 $ str)
+    | "dikunt: " `isPrefixOf` str = return $ Just (drop 8 $ str)
     | otherwise = return Nothing
