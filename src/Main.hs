498cf209252d15fa89baff84dc54c375240b4a90
Bus000 <magnus@stavngaard.dk>
2016-09-25 10:42:02 +0200

    Implement better command line handling
    The command line is still not perfect. Many of the fields is not used
    and fields that should be required is not.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,33 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main (main) where
 
 import qualified Bot
 import Control.Exception
-import System.Environment
+import System.Console.CmdArgs
+
+data Dikunt = Dikunt
+  { server   :: String
+  , username :: String
+  , password :: String
+  , channel  :: String
+  } deriving (Data, Typeable, Show, Eq)
+
+dikunt :: Dikunt
+dikunt = Dikunt
+  { server = def &= help "Server to connect to"
+  , username = def &= help "Username to use"
+  , password = def &= help "Password to use"
+  , channel = def &= help "Channel to connect to"
+  } &=
+    help "Bot to run on IRC channels" &=
+    summary "Dikunt v0.0.0.0 (C) Magnus Stavngaard"
+
+mode :: IO Dikunt
+mode = cmdArgs dikunt
 
 main :: IO ()
 main = do
-    (password:_) <- getArgs
-    bracket (Bot.connect password) Bot.disconnect Bot.loop
+    args <- mode
+    let pass = password args
+
+    bracket (Bot.connect pass) Bot.disconnect Bot.loop
