8eacba2bea564e5e5464abde471d289c28be6fd1
Bus000 <magnus@stavngaard.dk>
2016-09-26 20:59:33 +0200

    Remove hardcoded nick

--- src/Functions/Fix.hs
+++ src/Functions/Fix.hs
@@ -6,20 +6,22 @@ import qualified Data.List as L
 import qualified BotTypes as BT
 import qualified Data.String.Utils as SU
 
-fix :: String -> String -> Maybe String
-fix str lastmsg
-    | "dikunt: fix " `L.isPrefixOf` str = case words str' of
+{- TODO: for some reason this does not work. Figure out why. -}
+fix :: String -> String -> String -> Maybe String
+fix str lastmsg nick
+    | (nick ++ ": fix ") `L.isPrefixOf` str = case words str' of
         [w1, "=", w2] -> Just $ SU.replace w1 w2 lastmsg
         _ -> Nothing
     | otherwise = Nothing
   where
-    str' = drop (length "dikunt: fix ") str
+    str' = drop (length $ nick ++ ": fix ") str
 
 runFix :: BT.BotFunction
 runFix = do
     lastMsg <- BT.getValue BT.lastMessage
     msg <- BT.getValue BT.message
+    nick <- BT.getValue BT.nickname
 
     case lastMsg of
         Nothing -> return Nothing
-        Just l -> return $ fix msg l
+        Just l -> return $ fix msg l nick
