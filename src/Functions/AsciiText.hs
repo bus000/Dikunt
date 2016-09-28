71a526251973f9fb90ee6e7c0d89aa6f6c81c058
Jan Meznik <jan@meznik.dk>
2016-09-28 11:54:54 +0200

    Added AsciiText, but BotFunctions need to be fixed before enabling.
    AsciiText implemented, but since it can take any text input, it will be
    called any time by BotFunctions.
    
    Needs to be fixed.

---
+++ src/Functions/AsciiText.hs
@@ -0,0 +1,22 @@
+module Functions.AsciiText
+  ( runAsciiText
+  ) where
+
+import qualified BotTypes as BT
+import Control.Monad.State
+import Data.List
+import System.Exit
+import System.Process
+
+runAsciiText :: BT.BotFunction
+runAsciiText = do
+    str <- BT.getValue BT.message
+    asciiText str
+
+
+asciiText :: String -> BT.Net (Maybe String)
+asciiText str = do
+    (e,s,_) <- liftIO $ readProcessWithExitCode "/usr/bin/toilet" [str] []
+    if e /= ExitSuccess
+        then return Nothing
+        else return $ Just s
