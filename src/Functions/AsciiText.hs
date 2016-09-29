18e079218fbb4069f30c76c4eb785338d13b4d0e
Bus000 <magnus@stavngaard.dk>
2016-09-29 09:00:59 +0200

    Don't include asciitext: in output

--- src/Functions/AsciiText.hs
+++ src/Functions/AsciiText.hs
@@ -17,8 +17,10 @@ runAsciiText = do
 asciiText :: String -> BT.Net (Maybe String)
 asciiText str
     | "asciitext: " `isPrefixOf` str = do
-        (e,s,_) <- liftIO $ readProcessWithExitCode "/usr/bin/toilet" [str] []
+        (e,s,_) <- liftIO $ readProcessWithExitCode "/usr/bin/toilet" [str'] []
         if e /= ExitSuccess
             then return Nothing
             else return $ Just s
     | otherwise = return Nothing
+  where
+    str' = drop (length "asciitext: ") str
