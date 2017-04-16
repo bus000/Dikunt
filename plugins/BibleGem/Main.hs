e0c6750c405013f7ccf618bed0f68ab904db5b7f
Bus000 <magnus@stavngaard.dk>
2017-04-16 22:32:41 +0200

    Make BibleGem new kind of plugin

---
+++ plugins/BibleGem/Main.hs
@@ -0,0 +1,53 @@
+module Main where
+
+import qualified BotTypes as BT
+import Data.ByteString.Char8 (unpack)
+import Network.Download (openURI)
+import Text.Regex.PCRE ((=~))
+import System.Environment (getArgs)
+import System.IO (stdout, stdin, hSetBuffering, BufferMode(..))
+import Control.Monad (forever)
+import Safe (readMay)
+
+main :: IO ()
+main = do
+    [nick, chan] <- getArgs
+
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stdin LineBuffering
+
+    forever $ do
+        line <- getLine
+        output <- biblegem nick line
+
+        case output of
+            Just str -> putStrLn str
+            Nothing -> return ()
+
+biblegem :: String -> String -> IO (Maybe String)
+biblegem nick s = case readMay s :: Maybe BT.Message of
+    Just (BT.PrivMsg _ _ str) -> if str =~ pattern
+        then getGem
+        else return . return $ concat ["did not match ", str, " with ", pattern]
+    _ -> return Nothing
+  where
+    pattern = concat ["^", sp, nick, "\\:", ps, "biblegem", sp, "$"]
+    sp = "[ \\t]*"
+    ps = "[ \\t]+"
+
+getGem :: IO (Maybe String)
+getGem = do
+    res <- openURI randomQuote
+    case res of
+        Left err -> return Nothing
+        Right passage -> return $ Just (format (unpack passage))
+
+format :: String -> String
+format passage = case passage =~ pattern :: [[String]] of
+    [[_, verse, content]] -> verse ++ ": " ++ content
+    _ -> passage
+  where
+    pattern = "^<b>(.*)<\\/b> (.*)$"
+
+randomQuote :: String
+randomQuote = "http://labs.bible.org/api/?passage=random"
