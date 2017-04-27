819b1c66b8ab8d91d63b223e1becb72cd765b8ec
Bus000 <magnus@stavngaard.dk>
2017-04-27 11:25:01 +0200

    Insert default replacements when bot is started
    Also add comments and refactor plugin.

--- plugins/WordReplacer/Main.hs
+++ plugins/WordReplacer/Main.hs
@@ -29,38 +29,65 @@ main = do
     hSetBuffering stdin LineBuffering
 
     dbFile <- getDataFileName "data/WordReplacerData.db"
-    DB.withConnection dbFile (wordReplacer nick)
+    DB.withConnection dbFile (\c -> initDatabase c >> wordReplacer nick c)
 
-wordReplacer :: String -> DB.Connection -> IO ()
-wordReplacer nick conn = do
+{- | Initialize a database by creating it if it does not exist and inserting
+ - default mappings. -}
+initDatabase :: DB.Connection
+    -- ^ Connection to database to initialize.
+    -> IO ()
+initDatabase conn = do
     DB.execute_ conn "CREATE TABLE IF NOT EXISTS replacements \
         \(id INTEGER PRIMARY KEY, word TEXT UNIQUE, replacement TEXT)"
+    mapM_ (insertDefault conn) replacementList
+  where
+    insertDefault c (word, replacement) =
+        DB.executeNamed conn "INSERT OR REPLACE INTO replacements \
+            \(word, replacement) VALUES (:word, :replacement)"
+                [":word" := word
+                , ":replacement" := replacement
+                ]
 
+{- | Do the word replacements. Reads lines from stdin replaces words and prints
+ - the replacement if any to stdout. -}
+wordReplacer :: String
+    -- ^ Nickname of Dikunt bot.
+    -> DB.Connection
+    -- ^ Database connection with replacements.
+    -> IO ()
+wordReplacer nick conn = do
     forever $ do
         line <- getLine
-        case readMay line :: Maybe BT.ServerMessage of
-            Just (BT.ServerPrivMsg _ _ str) -> handleInput conn nick str
-            _ -> return ()
+        handleMessage conn nick $ readMay line
 
-handleInput :: DB.Connection -> String -> String -> IO ()
-handleInput conn nick str
+{- | Parse message and determine if it is a help message, an add message or if
+ - the words should be replaced. -}
+handleMessage :: DB.Connection
+    -- ^ Database connection.
+    -> String
+    -- ^ Nickname of Dikunt bot.
+    -> Maybe BT.ServerMessage
+    -- ^ Message received from IRCServer.
+    -> IO ()
+handleMessage conn nick (Just (BT.ServerPrivMsg _ _ str))
     | str =~ helpPattern nick = help nick
     | str =~ addPattern nick = case str =~ addPattern nick of
         [[_, word, replacement]] -> addReplacement conn word replacement
         _ -> return ()
     | otherwise = replaceWords conn str
+  where
+    sp = "[ \\t]*"
+    ps = "[ \\t]+"
+    helpPattern nick = concat ["^", sp, nick, "\\:", ps, "wordreplacer", ps, "help",
+        sp, "$"]
+    addPattern nick = concat ["^", sp, nick, "\\:", ps, "wordreplacer", ps, "add",
+        ps, "([a-zA-Z0-9]*)=([a-zA-Z0-9]*)"]
+handleMessage _ _ _ = return ()
 
-sp, ps :: String
-sp = "[ \\t]*"
-ps = "[ \\t]+"
-
-helpPattern, addPattern :: String -> String
-helpPattern nick = concat ["^", sp, nick, "\\:", ps, "wordreplacer", ps, "help",
-    sp, "$"]
-addPattern nick = concat ["^", sp, nick, "\\:", ps, "wordreplacer", ps, "add",
-    ps, "([a-zA-Z0-9]*)=([a-zA-Z0-9]*)"]
-
-help :: String -> IO ()
+{- | Print help message. -}
+help :: String
+    -- ^ Nickname of Dikunt bot.
+    -> IO ()
 help nick = putStrLn $ unlines
     [ unwords [nick, "wordreplacer add <word1>=<word2> - add word1=word2 to"
         , "database"]
@@ -68,7 +95,14 @@ help nick = putStrLn $ unlines
     , "otherwise replaces words from database in messages"
     ]
 
-addReplacement :: DB.Connection -> String -> String -> IO ()
+{- | Add a new replacement to the database. -}
+addReplacement :: DB.Connection
+    -- ^ Connection to database.
+    -> String
+    -- ^ The word to replace.
+    -> String
+    -- ^ The replacement for the word.
+    -> IO ()
 addReplacement conn word replacement = do
     DB.executeNamed conn "INSERT OR REPLACE INTO replacements \
         \(word, replacement) VALUES (:word, :replacement)" [":word" := word,
@@ -76,7 +110,12 @@ addReplacement conn word replacement = do
 
     putStrLn "Added replacement"
 
-replaceWords :: DB.Connection -> String -> IO ()
+{- | Replace all words present in the database with their replacements. -}
+replaceWords :: DB.Connection
+    -- ^ Database connection.
+    -> String
+    -- ^ String to replace in.
+    -> IO ()
 replaceWords conn line = do
     replacements <- foldM getReplacements [] (words line)
     if null replacements
@@ -89,7 +128,13 @@ replaceWords conn line = do
             [":word" := word] :: IO [Replacement]
         if null r then return replacements else return $ (head r):replacements
 
-replaceStrings :: [Replacement] -> String -> String
+{- | Replace words in string with their replacement in the replacementlist
+ - given. -}
+replaceStrings :: [Replacement]
+    -- ^ Word replacements.
+    -> String
+    -- ^ The string to replace in.
+    -> String
 replaceStrings replacements str = unwords . map maybeReplace . words $ str
   where
     replacementList = map getReplacement replacements
@@ -97,45 +142,48 @@ replaceStrings replacements str = unwords . map maybeReplace . words $ str
         Just newWord -> newWord
         Nothing -> word
 
-getReplacement :: Replacement -> (String, String)
+{- | Utility function converting Replacement's to tuples of replacement. -}
+getReplacement :: Replacement
+    -- ^ The replacements.
+    -> (String, String)
 getReplacement (Replacement _ word replacement) =
     (T.unpack word, T.unpack replacement)
 
-{- TODO: Make default replacements. -}
-{-replacementList :: [(String, String)]-}
-{-replacementList =-}
-    {-[ ("Mark", "ShortGuy")-}
-    {-, ("Jan", "Tjekkeren")-}
-    {-, ("Magnus", "Glorious")-}
-    {-, ("August", "Motherless")-}
-    {-, ("Oleks", "Joleks")-}
-    {-, ("10/10", "knæhøj karse")-}
-    {-, ("ha!", "HAHAHAHAHAHHAHA!")-}
-    {-, ("haha", "hilarious, just hilarious")-}
-    {-, ("Mads", "Twin")-}
-    {-, ("Troels", "Twin")-}
-    {-, ("tjekker", "kontrollerer")-}
-    {-, ("Tjekker", "Kontrollerer")-}
-    {-, ("tjekke", "kontrollere")-}
-    {-, ("vim", "best editor")-}
-    {-, ("emacs", "worst editor")-}
-    {-, ("fucking", "")-}
-    {-, ("fuck", "")-}
-    {-, ("Fucking", "")-}
-    {-, ("Fuck", "")-}
-    {-, ("Helvede", "Himlen")-}
-    {-, ("helvede", "himlen")-}
-    {-, ("0/10", "Mark's Bot")-}
-    {-, ("danmark", "I DANMARK ER JEG FØDT DER HAR JEG HJEEEEEME")-}
-    {-, ("Danmark", "I DANMARK ER JEG FØDT DER HAR JEG HJEEEEEME")-}
-    {-, ("USA", "Murica'")-}
-    {-, ("Margrethe", "Hendes Majestæt Dronning Margrethe II")-}
-    {-, ("Henrik", "Hans Kongelige Højhed Prins Henrik")-}
-    {-, ("Frederik", "Hans Kongelige Højhed Kronprins Frederik, Prins til Danmark, greve af Monpezat")-}
-    {-, ("Joakim", "Hans Kongelige Højhed Prins Joachim")-}
-    {-, ("Mary", "Hendes Kongelige Højhed Kronprinsesse Mary")-}
-    {-, ("python", "python2.7")-}
-    {-, ("Python", "Python2.7")-}
-    {-, ("latex", "LaTeX")-}
-    {-, ("tex", "Tex")-}
-    {-]-}
+{- | List of default replacements. -}
+replacementList :: [(String, String)]
+replacementList =
+    [ ("Mark", "ShortGuy")
+    , ("Jan", "Tjekkeren")
+    , ("Magnus", "Glorious")
+    , ("August", "Motherless")
+    , ("Oleks", "Joleks")
+    , ("10/10", "knæhøj karse")
+    , ("ha!", "HAHAHAHAHAHHAHA!")
+    , ("haha", "hilarious, just hilarious")
+    , ("Mads", "Twin")
+    , ("Troels", "Twin")
+    , ("tjekker", "kontrollerer")
+    , ("Tjekker", "Kontrollerer")
+    , ("tjekke", "kontrollere")
+    , ("vim", "best editor")
+    , ("emacs", "worst editor")
+    , ("fucking", "")
+    , ("fuck", "")
+    , ("Fucking", "")
+    , ("Fuck", "")
+    , ("Helvede", "Himlen")
+    , ("helvede", "himlen")
+    , ("0/10", "Mark's Bot")
+    , ("danmark", "I DANMARK ER JEG FØDT DER HAR JEG HJEEEEEME")
+    , ("Danmark", "I DANMARK ER JEG FØDT DER HAR JEG HJEEEEEME")
+    , ("USA", "Murica'")
+    , ("Margrethe", "Hendes Majestæt Dronning Margrethe II")
+    , ("Henrik", "Hans Kongelige Højhed Prins Henrik")
+    , ("Frederik", "Hans Kongelige Højhed Kronprins Frederik, Prins til Danmark, greve af Monpezat")
+    , ("Joakim", "Hans Kongelige Højhed Prins Joachim")
+    , ("Mary", "Hendes Kongelige Højhed Kronprinsesse Mary")
+    , ("python", "python2.7")
+    , ("Python", "Python2.7")
+    , ("latex", "LaTeX")
+    , ("tex", "Tex")
+    ]
