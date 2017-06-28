dff009274af0ff346fd7213c77c3e2fad7e7aa37
Magnus Stavngaard <magnus@stavngaard.dk>
2017-06-28 03:42:45 -0400

    Parse lines from the log

--- plugins/History/Main.hs
+++ plugins/History/Main.hs
@@ -1,15 +1,16 @@
 module Main (main) where
 
 import qualified BotTypes as BT
-import Control.Monad (foldM_, void)
+import Control.Monad (void)
 import Data.Aeson (decode)
 import Data.Maybe (mapMaybe)
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.Encoding as T
 import qualified Data.Text.Lazy.IO as T
-import Prelude hiding (lines)
+import Paths_Dikunt (getDataFileName)
+import Safe (readMay)
 import System.Environment (getArgs)
-import System.IO (stdout, stdin, hSetBuffering, BufferMode(..))
+import System.IO (stdout, stdin, hSetBuffering, BufferMode(..), readFile)
 import Text.ParserCombinators.ReadP
     ( ReadP
     , eof
@@ -19,9 +20,13 @@ import Text.ParserCombinators.ReadP
     , string
     , char
     , munch
+    , munch1
+    , pfail
+    , between
+    , many
+    , satisfy
+    , look
     )
-
-type State = ()
 
 data Request = HelpRequest | GetRequest Position Position
 
@@ -39,18 +44,61 @@ main = do
     hSetBuffering stdout LineBuffering
     hSetBuffering stdin LineBuffering
 
+    privmsgs <- getDataFileName "data/privmsgLog.db"
+    lineN <- length . lines <$> readFile privmsgs
+
     messages <- parseMessages nick <$> T.hGetContents stdin
 
-    -- TODO: should not be 0.
-    foldM_ (handleMessage nick) () $ zip [0..] messages
+    mapM_ (handleMessage nick privmsgs) $ zip [lineN..] messages
 
-handleMessage :: BT.Nickname -> State -> (Int, Maybe Request) -> IO State
+handleMessage :: BT.Nickname -> FilePath -> (Int, Maybe Request) -> IO ()
 handleMessage _ _ (_, Nothing) = return ()
 handleMessage nick _ (_, Just HelpRequest) = putStrLn $ helpString nick
-handleMessage _ _ (messageNumber, Just (GetRequest startPos endPos)) =
+handleMessage _ file (messageNumber, Just (GetRequest startPos endPos)) =
     case evaluatePositions messageNumber startPos endPos of
-        Right (start, end) -> putStrLn $ "start " ++ show start ++ " end " ++ show end
+        Right (start, end) -> do
+            str <- parseLines <$> getLines file start end
+            putStrLn str
         Left err -> putStrLn err
+
+parseLines :: [String] -> String
+parseLines = unlines . map parseLine
+  where
+    parseLine line = case readP_to_S parser line of
+        [(content, "")] -> content
+        x -> show x -- TODO: should return nothing.
+
+    parser = do
+        time <- between (char '[') (char ']') parseTime
+        void $ char ' '
+        (user, message) <- parseServer +++ parseClient
+        eof
+        return $ time ++ " " ++ user ++ ": " ++ message
+
+    parseTime = do
+        yearMonthDay <- munch1 ((/=) ' ')
+        hourMinutSecond <- munch1 ((/=) ' ')
+        void (many $ satisfy (\_ -> True))
+        return $ yearMonthDay ++ " " ++ hourMinutSecond ++ " EDT"
+
+    parseServer = do
+        input <- look
+        case readMay input of
+            Just (BT.ServerPrivMsg (BT.IRCUser user _ _) _ message) ->
+                return (user, message)
+            _ -> pfail
+
+    parseClient = do
+        input <- look
+        case readMay input of
+            Just (BT.ClientPrivMsg (BT.IRCUser _ _ _) _) ->
+                return ("dikunt", "Redacted")
+            Just (BT.ClientPrivMsgChan _ message) ->
+                return ("dikunt", message)
+            _ -> pfail
+
+getLines :: String -> Int -> Int -> IO [String]
+getLines file start end = take (end - start) . drop start . lines <$> readFile file
 
 evaluatePositions :: Int -> Position -> Position -> Either String (Int, Int)
 evaluatePositions lastline pos1 pos2
