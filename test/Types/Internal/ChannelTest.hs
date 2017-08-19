215c52eab2a9ea865d346c0bd8cc0611796eab2c
Magnus Stavngaard <magnus@stavngaard.dk>
2017-08-19 02:19:15 -0400

    Testing

--- test/Types/Internal/ChannelTest.hs
+++ test/Types/Internal/ChannelTest.hs
@@ -3,12 +3,15 @@ module Types.Internal.ChannelTest (tests) where
 import Data.Aeson (encode, decode)
 import Test.Tasty (TestTree , testGroup)
 import Test.Tasty.QuickCheck (testProperty)
-import Types.Internal.Channel (Channel, getChannel)
+import Types.Internal.Channel (Channel, getChannel, channel)
 
 tests :: TestTree
 tests = testGroup "Channel Tests"
-    [ testProperty "json identity" jsonIdent
-    , testProperty "starts with correct" startsWith
+    [ testProperty "jsonIdent" jsonIdent
+    , testProperty "startsWith" startsWith
+    , testProperty "minSize" minSize
+    , testProperty "getChannelIdent" getChannelIdent
+    , testProperty "dontContain" dontContain
     ]
 
 jsonIdent :: Channel -> Bool
@@ -16,3 +19,12 @@ jsonIdent chan = Just chan == (decode . encode) chan
 
 startsWith :: Channel -> Bool
 startsWith chan = (head . getChannel) chan `elem` "#+&"
+
+minSize :: Channel -> Bool
+minSize chan = (length . getChannel) chan >= 2
+
+getChannelIdent :: Channel -> Bool
+getChannelIdent chan = Just chan == (channel . getChannel) chan
+
+dontContain :: Channel -> Bool
+dontContain chan = not $ any (`elem` "\0\a\r\n ,:") (getChannel chan)
