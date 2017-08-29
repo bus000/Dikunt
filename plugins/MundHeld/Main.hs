b2e1cf08eaf3d86e364c7fa993f821d303fbcca4
Magnus Stavngaard <magnus@stavngaard.dk>
2017-08-29 05:18:56 -0400

    Add more mundhelds

--- plugins/MundHeld/Main.hs
+++ plugins/MundHeld/Main.hs
@@ -48,6 +48,37 @@ giveMundHeld search = case filter (search `isInfixOf`) mundHelds of
     [] -> putStrLn "Jeg fandt ingen mundheld der matchede din søge streng."
     (x:_) -> putStrLn x
 
+parseRequests :: User -> T.Text -> [Request]
+parseRequests botnick =
+    mapMaybe getRequest . mapMaybe (decode . T.encodeUtf8) . T.lines
+  where
+    getRequest (BT.ServerPrivMsg _ _ msg) =
+        hush $ P.parse (request botnick) "(stdin)" (BT.getMessage msg)
+    getRequest _ = Nothing
+
+request :: User -> P.Parsec String () Request
+request botnick = do
+    void $ token (P.string $ botnick ++ ":")
+    void $ token (P.string "mundheld")
+    req <- P.try (helpRequest botnick) <|> P.try mundHeldRequest
+    P.eof
+
+    return req
+
+helpRequest :: User -> P.Parsec String () Request
+helpRequest botnick = token (P.string "help") *> return (Help botnick)
+
+mundHeldRequest :: P.Parsec String () Request
+mundHeldRequest = GetMundHeld . trim <$> P.many1 (P.noneOf "\r\n")
+
+token :: P.Parsec String () a -> P.Parsec String () a
+token tok = P.spaces *> tok <* P.spaces
+
+trim :: String -> String
+trim = f . f
+  where
+    f = reverse . dropWhile isSpace
+
 {- Sources of mundheld: http://www.udvalgte-ordsprog.dk/danske-ordsprog.htm -}
 mundHelds :: [String]
 mundHelds =
@@ -96,35 +127,55 @@ mundHelds =
     , "Klog hund tisser ikke mod vinden"
     , "Bedre sent end aldrig"
     , "Den sidste idiot, er ikke født endnu"
+    , "De værste sår er de, der ikke bløder"
+    , "En dåre kan spørge om mere, end ti vise kan svare på"
+    , "Den der sladrer til dig sladrer også om dig"
+    , "Den der tier samtykker"
+    , "At tale er at så, at lytte er at høste"
+    , "Den der ler sidst ler bedst"
+    , "At falde er ingen skam, men at blive liggende er"
+    , "Farlig er den, som intet har at miste"
+    , "Sjældent kommer lånte penge smilende tilbage"
+    , "Det er menneskeligt at fejle, men idiotisk at blive ved"
+    , "En sand ven kommer ind, når andre går ud"
+    , "Det man i barndommen nemmer, man ej i alderdommen glemmer"
+    , "Man kan lære umådeligt meget af de gamle ordsprog, men man gør det ikke"
+    , "Helbredet værdsættes ikke, før sygdommen kommer"
+    , "Arbejd mens du er frisk og ung, det gavner, når du bliver gammel og tung"
+    , "Den enes død, den andens brød"
+    , "Der er aldrig noget så galt, at det ikke er godt for noget"
+    , "Der går ikke røg af en brand uden ild"
+    , "Det er dyrt at være fattig"
+    , "Det er i nøden man skal kende sine venner"
+    , "Bagefter er vi alle kloge"
+    , "Alting er let, når bare man kan det"
+    , "Den man elsker tugter man"
+    , "En ny kost fejer rent, men en gammel kost kender krogene"
+    , "Tid er penge"
+    , "Tiden læger alle sår"
+    , "Glem ikke, at dem, der elsker dig mest, også er dem, du kan såre dybest"
+    , "Det er med sovende gæster som med fisk – de stinker på tredjedagen"
+    , "Gud skabte kvinden, og hun har skabt sig siden!"
+    , "I de blindes rige er den enøjede konge"
+    , "Hellere have system i rodet end rod i systemet!"
+    , "Et råddent æg fordærver hele kagen"
+    , "Der findes tre slags mennesker; dem der kan tælle og dem der ikke kan!"
+    , "Lad os nu se, sagde den blinde til den døve."
+    , "Et er at have ret, et andet at få ret"
+    , "Bed om råd, men brug din egen sunde fornuft"
+    , "Bedre et ærligt nej end et falsk ja"
+    , "Bedst at udstå, hvad man ikke kan undgå"
+    , "Skomager bliv ved din læst"
+    , "Det er for sent at kaste brønden til, når barnet er druknet"
+    , "Det er hård kost at æde sine egne ord"
+    , "Det er ikke de smukke vi elsker, men dem vi elsker, som er smukke…"
+    , "Gammel kærlighed ruster ikke"
+    , "At undervise er at lære en gang til"
+    , "At forsvare en fejl er at fejle igen"
+    , "Det, der kommer let, går let"
+    , "Dit liv er, hvad dine tanker gør det til"
+    , "Der findes ikke noget du ikke kan – kun ting du ikke har lært endnu"
+    , "Helbred er hvordan man har det – modstands kraft er hvordan man tar' det"
+    , "I morgen er første dag i resten af dit liv"
+    , "Et smil åbner mange døre"
     ]
-
-parseRequests :: User -> T.Text -> [Request]
-parseRequests botnick =
-    mapMaybe getRequest . mapMaybe (decode . T.encodeUtf8) . T.lines
-  where
-    getRequest (BT.ServerPrivMsg _ _ msg) =
-        hush $ P.parse (request botnick) "(stdin)" (BT.getMessage msg)
-    getRequest _ = Nothing
-
-request :: User -> P.Parsec String () Request
-request botnick = do
-    void $ token (P.string $ botnick ++ ":")
-    void $ token (P.string "mundheld")
-    req <- P.try (helpRequest botnick) <|> P.try mundHeldRequest
-    P.eof
-
-    return req
-
-helpRequest :: User -> P.Parsec String () Request
-helpRequest botnick = token (P.string "help") *> return (Help botnick)
-
-mundHeldRequest :: P.Parsec String () Request
-mundHeldRequest = GetMundHeld . trim <$> P.many1 (P.noneOf "\r\n")
-
-token :: P.Parsec String () a -> P.Parsec String () a
-token tok = P.spaces *> tok <* P.spaces
-
-trim :: String -> String
-trim = f . f
-  where
-    f = reverse . dropWhile isSpace
